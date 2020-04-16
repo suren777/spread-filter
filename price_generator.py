8e0d2b089f9eb83f67d8cfe9a1bacde2e92059c1
Suren Islyaev <suren.islyaev@gmail.com>
2020-04-16 14:55:27 +0100

    Adding calenda spreads to the picture

--- price_generator.py
+++ price_generator.py
@@ -39,20 +39,32 @@ def xtdt(x0, a, sig, k, dt, w):
     return B(k, dt) * x0 + g(a, k, dt) + np.sqrt(R(sig, k, dt) * dt) * w
 
 
-def generate_path(x0, a, l, sig, k, dt, Tn, c1, c2, c3, c4, eta, n_steps=100):
+def log_futures(spot, T, k, a, l, sig, eta, c1, c2, c3, c4, q_noise):
+    return (
+        An(T, k) * spot + dn(a, l, sig, k, T, c1, c2, c3, c4) + q_noise * eta
+    )
+
+
+def calendar_spread(spot, T1, T2, k, a, l, sig, eta, c1, c2, c3, c4, q_noise):
+    f1 = An(T1, k) * spot + dn(a, l, sig, k, T1, c1, c2, c3, c4)
+    f2 = An(T2, k) * spot + dn(a, l, sig, k, T2, c1, c2, c3, c4)
+    return np.exp(f1) - np.exp(f2) + q_noise * eta
+
+
+def generate_futures_path(
+    x0, a, l, sig, k, dt, Tn, c1, c2, c3, c4, eta, n_steps=100
+):
     n_fut = len(Tn)
-    w = np.random.normal(size=n_steps)
+    wiener = np.random.normal(size=n_steps)
     q = np.random.normal(size=(n_fut, n_steps))
     spot = np.zeros(shape=n_steps + 1)
     futures = np.zeros(shape=(n_fut, n_steps))
     spot[0] = x0
     T = np.array(Tn)
-    for i, W in enumerate(w):
-        spot[i + 1] = xtdt(spot[i], a, sig, k, dt, w[i])
-        futures[:, i] = (
-            An(T, k) * spot[i]
-            + dn(a, l, sig, k, T, c1, c2, c3, c4)
-            + q[:, i] * eta
+    for i, w in enumerate(wiener):
+        spot[i + 1] = xtdt(spot[i], a, sig, k, dt, w)
+        futures[:, i] = log_futures(
+            spot[i], T, k, a, l, sig, eta, c1, c2, c3, c4, q[:, i]
         )
         for i, t in enumerate(list(T)):
             if T[i] - dt >= 0:
@@ -60,6 +72,28 @@ def generate_path(x0, a, l, sig, k, dt, Tn, c1, c2, c3, c4, eta, n_steps=100):
             else:
                 T[i] = Tn[i]
     return spot, futures
+
+
+def generate_spread_path(
+    x0, a, l, sig, k, dt, Tn, c1, c2, c3, c4, eta, n_steps=100
+):
+    wiener = np.random.normal(size=n_steps)
+    q = np.random.normal(size=n_steps)
+    spot = np.zeros(shape=n_steps + 1)
+    spreads = np.zeros(shape=n_steps)
+    spot[0] = x0
+    T = np.array(Tn)
+    for i, w in enumerate(wiener):
+        spot[i + 1] = xtdt(spot[i], a, sig, k, dt, w)
+        spreads[i] = calendar_spread(
+            spot[i], T[0], T[1], k, a, l, sig, eta, c1, c2, c3, c4, q[i]
+        )
+        for i, t in enumerate(list(T)):
+            if T[i] - dt >= 0:
+                T[i] -= dt
+            else:
+                T[i] = Tn[i]
+    return spot, spreads
 
 
 def one_factor_model():
